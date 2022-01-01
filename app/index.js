42f90359350ae50b861fa62b5840cc52f585c80f
Moch Arfian <mocharfian1@gmail.com>
2022-01-01 17:42:23 +0700

    Test

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.get('/', (req, res) => res.send('<h2> Moch Arfian Ardiansyah </h2>'))
+app.get('/', (req, res) => {
+    res.send('<h2> Moch Arfian Ardiansyah </h2>')
+})
 
-app.listen(PORT, () => console.log(`Aplikasi berjalan di port : ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Aplikasi berjalan di port : ${PORT}`)
+})
